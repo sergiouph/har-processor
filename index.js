874aaccca2e924feac6cb7d3a3bfa78b5cab9077
sergiouph <sergio.uriel.ph@gmail.com>
2018-09-27 17:41:57 -0500

    Add hosts.txt file processing

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const url = require('url');
 
 const homedir = os.homedir();
 const folder = `${homedir}/Desktop/hars`; // should contain *.har files
-const outputFile = `${folder}/summary.json`;
+const summaryFile = `${folder}/summary.json`;
+const hostsFile = `${folder}/hosts.txt`;
 
 const summary = {};
 
@@ -35,8 +36,58 @@ for (const file of fs.readdirSync(folder).filter(f => f.endsWith('.har'))) {
   }
 }
 
-console.log(`generating ${outputFile}...`);
+let hosts = "";
 
-fs.writeFileSync(outputFile, JSON.stringify(summary, null, 2));
+for (const domain in summary) {
+  let unique_ip = true;
+  let domain_ip = null;
+
+  for (const path in summary[domain]) {
+    const ip = summary[domain][path];
+
+    if (ip && ip.length > 0) {
+      if (domain_ip === null) {
+        domain_ip = ip;
+      }
+      else if(domain_ip !== ip) {
+        unique_ip = false;
+      }
+    }
+  }
+
+  if (unique_ip && domain_ip !== null && domain_ip.length > 0) {
+    hosts += `${domain}\t${domain_ip}\n`;
+  }
+  else if (Object.keys(summary[domain]).length > 0) {
+    hosts += '\n';
+    hosts += `# '${domain}' special cases:\n`;
+
+    for (const path in summary[domain]) {
+      const ip = summary[domain][path];
+
+      if (ip && ip.length > 0) {
+        hosts += `${domain}${path}\t${ip}\n`;
+      }
+      else {
+        hosts += `# ${domain}${path}\t(no ip found)\n`;
+      }
+    }
+
+    hosts += '\n';
+  }
+  else {
+    hosts += '\n';
+    hosts += `# ${domain}\t(no path found)\n`;
+    hosts += '\n';
+  }
+}
+
+console.log(`generating ${summaryFile}...`);
+
+fs.writeFileSync(summaryFile, JSON.stringify(summary, null, 2));
+
+console.log(`generating ${hostsFile}...`);
+
+fs.writeFileSync(hostsFile, hosts);
 
 console.log(`done!`);
